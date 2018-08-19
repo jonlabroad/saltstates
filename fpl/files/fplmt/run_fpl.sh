32a6beebbc638d320f4059c962f297cbb011ab67
Jonathan LaBroad <jon.labroad@gmail.com>
2018-08-19 10:02:14 -0400

    aws

--- fpl/files/fplmt/run_fpl.sh
+++ fpl/files/fplmt/run_fpl.sh
@@ -5,7 +5,7 @@ set -x
 echo "Running now..."
 
 echo "/usr/local/bin/aws s3 sync s3://fantasyeplmatchtracker/bin /home/pi/"
-/usr/bin/aws s3 sync s3://fantasyeplmatchtracker/bin /home/pi/
+/usr/local/bin/aws s3 sync s3://fantasyeplmatchtracker/bin /home/pi/
 echo "Here"
 
 echo "/usr/bin/java -cp /home/pi/FantasyEPLMonitor.jar lambda.AllProcessorLambda"
